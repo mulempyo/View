ba809fe592f049748fa0adfa766f2fafa293c766
codchild <luckyp1@naver.com>
2021-09-06 22:19:04 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/viewapplication/MainActivity.kt
@@ -0,0 +1,38 @@
+package org.techtown.viewapplication
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import org.techtown.viewapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val customView = CustomView("안녕 코틀린!",this)
+        binding.frameLayout.addView(customView)
+    }
+}
+
+class CustomView(text : String , context : Context): View(context){
+    val text : String
+    init{
+        this.text=text
+    }
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        
+        val paint = Paint()
+        paint.color = Color.BLACK
+        paint.textSize = 100f
+        canvas?.drawText(text,100f,300f,paint) //drawText메서드
+
+    }
+
+}
